d8351add9cb4044ccf26a3f20714fa3ab719984f
Gregory <gregory@Gregorys-MacBook-Pro.local>
2011-05-05 10:42:27 +0200

    Added variable resolution

--- src/main/java/hudson/plugins/msbuild/MsBuildBuilder.java
+++ src/main/java/hudson/plugins/msbuild/MsBuildBuilder.java
@@ -11,31 +11,19 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
- * Sample {@link Builder}.
- * <p/>
- * <p/>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link MsBuildBuilder} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
- * <p/>
- * <p/>
- * When a build is performed, the {@link #perform(Build, Launcher, BuildListener)} method
- * will be invoked.
- *
  * @author kyle.sweeney@valtech.com
- *         2009/03/01 -- Gregory Boissinot - Zenika - Add the possibility to manage multiple Msbuild version
- *         2009/05/20 - Fix #3610
+ * @author Gregory Boissinot - Zenika
+ *         2009/03/01 - Added the possibility to manage multiple Msbuild version
+ *         2009/05/20 - Fixed #3610
+ *         2010/04/02 - Fixed #4121
+ *         2010/05/05 - Added environment and build variables resolving in fields
  */
 public class MsBuildBuilder extends Builder {
 
     /**
-     * Identifies {@link Visual Studio} to be used.
+     * GUI fields
      */
     private final String msBuildName;
     private final String msBuildFile;
@@ -45,25 +33,29 @@ public class MsBuildBuilder extends Builder {
      * When this builder is created in the project configuration step,
      * the builder object will be created from the strings below.
      *
-     * @param msName      The Visual studio logical identifiant name
+     * @param msBuildName The Visual studio logical name
      * @param msBuildFile The name/location of the msbuild file
-     * @param targets     Whitespace separated list of command line arguments
+     * @param cmdLineArgs Whitespace separated list of command line arguments
      */
     @DataBoundConstructor
+    @SuppressWarnings("unused")
     public MsBuildBuilder(String msBuildName, String msBuildFile, String cmdLineArgs) {
         this.msBuildName = msBuildName;
         this.msBuildFile = msBuildFile;
         this.cmdLineArgs = cmdLineArgs;
     }
 
+    @SuppressWarnings("unused")
     public String getCmdLineArgs() {
         return cmdLineArgs;
     }
 
+    @SuppressWarnings("unused")
     public String getMsBuildFile() {
         return msBuildFile;
     }
 
+    @SuppressWarnings("unused")
     public String getMsBuildName() {
         return msBuildName;
     }
@@ -77,7 +69,7 @@ public class MsBuildBuilder extends Builder {
     }
 
     @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException {
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
         ArgumentListBuilder args = new ArgumentListBuilder();
 
         String execName = "msbuild.exe";
@@ -101,32 +93,32 @@ public class MsBuildBuilder extends Builder {
             args.add(pathToMsBuild);
         }
 
-        //Remove all tabs, carriage returns, and newlines and replace them with
-        //whitespaces, so that we can add them as parameters to the executable
-        String normalizedTarget = cmdLineArgs.replaceAll("[\t\r\n]+", " ");
-        if (normalizedTarget.trim().length() > 0)
-            args.addTokenized(normalizedTarget);
+        EnvVars env = build.getEnvironment(listener);
+        String normalizedArgs = cmdLineArgs.replaceAll("[\t\r\n]+", " ");
+        normalizedArgs = Util.replaceMacro(normalizedArgs, env);
+        normalizedArgs = Util.replaceMacro(normalizedArgs, build.getBuildVariables());
+        if (normalizedArgs.trim().length() > 0)
+            args.addTokenized(normalizedArgs);
 
         args.addKeyValuePairs("-P:", build.getBuildVariables());
 
         //If a msbuild file is specified, then add it as an argument, otherwise
         //msbuild will search for any file that ends in .proj or .sln
         if (msBuildFile != null && msBuildFile.trim().length() > 0) {
-            args.add(msBuildFile);
+            String normalizedFile = msBuildFile.replaceAll("[\t\r\n]+", " ");
+            normalizedFile = Util.replaceMacro(normalizedFile, env);
+            normalizedFile = Util.replaceMacro(normalizedFile, build.getBuildVariables());
+            if (normalizedArgs.length() > 0)
+                args.add(normalizedFile);
         }
 
-        //According to the Ant builder source code, in order to launch a program 
-        //from the command line in windows, we must wrap it into cmd.exe.  This 
-        //way the return code can be used to determine whether or not the build failed.
         if (!launcher.isUnix()) {
             args.prepend("cmd.exe", "/C");
             args.add("&&", "exit", "%%ERRORLEVEL%%");
         }
 
-        //Try to execute the command
         listener.getLogger().println("Executing command: " + args.toStringWithQuote());
         try {
-            Map<String, String> env = build.getEnvironment(listener);
             int r = launcher.launch().cmds(args).envs(env).stdout(listener).pwd(build.getModuleRoot()).join();
             return r == 0;
         } catch (IOException e) {
