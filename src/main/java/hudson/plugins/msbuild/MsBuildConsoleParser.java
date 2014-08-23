4bd6dc07e6e1fd642ab6dd6d8437705d6f9ee8f9
Gregory Boissinot <gregory.boissinot@gmail.com>
2014-08-23 19:55:10 +0200

    Format

--- src/main/java/hudson/plugins/msbuild/MsBuildConsoleParser.java
+++ src/main/java/hudson/plugins/msbuild/MsBuildConsoleParser.java
@@ -17,57 +17,56 @@ import java.util.regex.Pattern;
 public class MsBuildConsoleParser extends LineTransformationOutputStream {
     private final OutputStream out;
     private final Charset charset;
-    
+
     private int numberOfWarnings = -1;
     private int numberOfErrors = -1;
-    
+
     public MsBuildConsoleParser(OutputStream out, Charset charset) {
         this.out = out;
         this.charset = charset;
     }
-    
+
     public int getNumberOfWarnings() {
         return numberOfWarnings;
     }
-    
+
     public int getNumberOfErrors() {
         return numberOfErrors;
     }
-    
+
     @Override
     protected void eol(byte[] b, int len) throws IOException {
         String line = charset.decode(ByteBuffer.wrap(b, 0, len)).toString();
-        
+
         // trim off CR/LF from the end
         line = trimEOL(line);
-        
+
         Pattern patternWarnings = Pattern.compile(".*\\d+\\sWarning\\(s\\).*");
         Pattern patternErrors = Pattern.compile(".*\\d+\\sError\\(s\\).*");
-        
+
         Matcher mWarnings = patternWarnings.matcher(line);
         Matcher mErrors = patternErrors.matcher(line);
-        
+
         if (mWarnings.matches()) { // Match the number of warnings
             String[] part = line.split(" ");
             try {
                 numberOfWarnings = Integer.parseInt(part[4]);
             } catch (NumberFormatException e) {
-                
+
             }
-        }
-        else if (mErrors.matches()) { // Match the number of errors
+        } else if (mErrors.matches()) { // Match the number of errors
             String[] part = line.split(" ");
             try {
                 numberOfErrors = Integer.parseInt(part[4]);
             } catch (NumberFormatException e) {
-                
+
             }
         }
-        
+
         // Write to output
-        out.write(b,0,len);
+        out.write(b, 0, len);
     }
-    
+
     @Override
     public void close() throws IOException {
         super.close();
